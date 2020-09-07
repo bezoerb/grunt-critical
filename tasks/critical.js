0a9bfbcef1e777ae9a5cc732821816ae263b21b9
Ben Zörb <benjamin.zoerb@jvm.de>
2020-09-08 00:01:52 +0200

    Strip newlines on tests

--- tasks/critical.js
+++ tasks/critical.js
@@ -15,159 +15,159 @@ const _ = require('lodash');
 const glob = require('glob');
 
 module.exports = grunt => {
-    /**
+  /**
      * Check wether a resource is external or not
      * @param href
      * @returns {boolean}
      */
-    function isExternal(href) {
-        return /(^\/\/)|(:\/\/)/.test(href);
-    }
+  function isExternal(href) {
+    return /(^\/\/)|(:\/\/)/.test(href);
+  }
 
-    grunt.registerMultiTask(
-        'critical',
-        'Extract & inline critical-path CSS from HTML',
-        function() {
-            const done = this.async();
-            const options = this.options({
-                // Your base directory
-                base: ''
-            });
+  grunt.registerMultiTask(
+    'critical',
+    'Extract & inline critical-path CSS from HTML',
+    function() {
+      const done = this.async();
+      const options = this.options({
+        // Your base directory
+        base: ''
+      });
 
-            process.setMaxListeners(0);
+      process.setMaxListeners(0);
 
-            // Loop files array
-            // Iterate over all specified file groups.
-            eachSeries(
-                this.files,
-                (f, next) => {
-                    options.base = path.normalize(options.base || '');
+      // Loop files array
+      // Iterate over all specified file groups.
+      eachSeries(
+        this.files,
+        (f, next) => {
+          options.base = path.normalize(options.base || '');
 
-                    // Make filepath absolute
-                    const absoluteBase = `${path.resolve(
-                        options.base || './'
-                    )}/`;
+          // Make filepath absolute
+          const absoluteBase = `${path.resolve(
+            options.base || './'
+          )}/`;
 
-                    // Concat specified files.
-                    let srcFiles = f.src.filter(filepath => {
-                        // Warn on and remove invalid source files (if nonull was set).
-                        if (
-                            !grunt.file.exists(filepath) &&
+          // Concat specified files.
+          let srcFiles = f.src.filter(filepath => {
+            // Warn on and remove invalid source files (if nonull was set).
+            if (
+              !grunt.file.exists(filepath) &&
                             !isExternal(filepath)
-                        ) {
-                            grunt.log.warn(
-                                `Source file "${filepath}" not found.`
-                            );
-                            return false;
-                        }
+            ) {
+              grunt.log.warn(
+                `Source file "${filepath}" not found.`
+              );
+              return false;
+            }
 
-                        return true;
-                    });
+            return true;
+          });
 
-                    srcFiles = srcFiles.concat(
-                        f.orig.src.filter(filepath => {
-                            return isExternal(filepath);
-                        })
-                    );
+          srcFiles = srcFiles.concat(
+            f.orig.src.filter(filepath => {
+              return isExternal(filepath);
+            })
+          );
 
-                    // nothing to do
-                    if (srcFiles.length === 0) {
-                        grunt.log.warn(
-                            `Destination (${f.dest}) not written because src files were empty.`
-                        );
-                        return;
-                    }
-
-                    if (srcFiles.length > 1 && !grunt.file.isDir(f.dest)) {
-                        grunt.log.warn(
-                            'Destination needs to be a directory for multiple src files'
-                        );
-                        return;
-                    }
-
-                    // use glob for css option
-                    if (options.css) {
-                        if (!Array.isArray(options.css)) {
-                            options.css = [options.css];
-                        }
-
-                        options.css = _.chain(options.css)
-                            .compact()
-                            .map(css => {
-                                return glob.sync(css, {
-                                    nosort: true
-                                });
-                            })
-                            .flatten()
-                            .value();
-                    }
-
-                    grunt.log.debug('SOURCE', srcFiles);
-                    grunt.log.debug('CSS', options.css);
-
-                    eachSeries(
-                        srcFiles,
-                        async src => {
-                            const inline = !/\.(css|scss|less|styl)/.test(
-                                path.extname(f.dest)
-                            );
-                            const options_ = {inline, ...options};
-
-                            if (isExternal(src)) {
-                                options_.src = src;
-                            } else {
-                                options_.src = path
-                                    .resolve(src)
-                                    .replace(absoluteBase, '');
-                            }
-
-                            let destination = f.dest;
-
-                            if (grunt.file.isDir(f.dest)) {
-                                destination = path.join(f.dest, options_.src);
-                            }
-
-                            grunt.log.debug('opts', options_);
-
-                            try {
-                                const {html, css} = await critical.generate(
-                                    options_
-                                );
-
-                                const output = inline ? html : css;
-                                const dirname = path.dirname(destination);
-
-                                if (!grunt.file.isDir(dirname)) {
-                                    grunt.file.mkdir(dirname);
-                                }
-
-                                grunt.file.write(
-                                    destination,
-                                    Buffer.from(output)
-                                );
-                                // Print a success message.
-                                grunt.log.ok(`File "${destination}" created.`);
-                                return output;
-                            } catch (error) {
-                                grunt.log.error(
-                                    `File "${destination}" failed.`,
-                                    error.message || error
-                                );
-                                throw error;
-                            }
-                        },
-                        error => {
-                            if (error) {
-                                grunt.fail.warn(`File "${f.dest}" failed.`);
-                                grunt.log.warn(error.message || error);
-                            }
-
-                            next();
-                        }
-                    );
-                },
-                done
+          // nothing to do
+          if (srcFiles.length === 0) {
+            grunt.log.warn(
+              `Destination (${f.dest}) not written because src files were empty.`
             );
-        }
-    );
+            return;
+          }
+
+          if (srcFiles.length > 1 && !grunt.file.isDir(f.dest)) {
+            grunt.log.warn(
+              'Destination needs to be a directory for multiple src files'
+            );
+            return;
+          }
+
+          // use glob for css option
+          if (options.css) {
+            if (!Array.isArray(options.css)) {
+              options.css = [options.css];
+            }
+
+            options.css = _.chain(options.css)
+              .compact()
+              .map(css => {
+                return glob.sync(css, {
+                  nosort: true
+                });
+              })
+              .flatten()
+              .value();
+          }
+
+          grunt.log.debug('SOURCE', srcFiles);
+          grunt.log.debug('CSS', options.css);
+
+          eachSeries(
+            srcFiles,
+            async src => {
+              const inline = !/\.(css|scss|less|styl)/.test(
+                path.extname(f.dest)
+              );
+              const options_ = {inline, ...options};
+
+              if (isExternal(src)) {
+                options_.src = src;
+              } else {
+                options_.src = path
+                  .resolve(src)
+                  .replace(absoluteBase, '');
+              }
+
+              let destination = f.dest;
+
+              if (grunt.file.isDir(f.dest)) {
+                destination = path.join(f.dest, options_.src);
+              }
+
+              grunt.log.debug('opts', options_);
+
+              try {
+                const {html, css} = await critical.generate(
+                  options_
+                );
+
+                const output = inline ? html : css;
+                const dirname = path.dirname(destination);
+
+                if (!grunt.file.isDir(dirname)) {
+                  grunt.file.mkdir(dirname);
+                }
+
+                grunt.file.write(
+                  destination,
+                  Buffer.from(output)
+                );
+                // Print a success message.
+                grunt.log.ok(`File "${destination}" created.`);
+                return output;
+              } catch (error) {
+                grunt.log.error(
+                  `File "${destination}" failed.`,
+                  error.message || error
+                );
+                throw error;
+              }
+            },
+            error => {
+              if (error) {
+                grunt.fail.warn(`File "${f.dest}" failed.`);
+                grunt.log.warn(error.message || error);
+              }
+
+              next();
+            }
+          );
+        },
+        done
+      );
+    }
+  );
 };
--- test/index.js
+++ test/index.js
@@ -7,140 +7,119 @@ const fs = require('fs');
 const nn = require('normalize-newline');
 const {expect} = require('chai');
 
-function readFile(file) {
-    const contents = fs.readFileSync(path.join(__dirname, file), 'utf8');
+function strip(string) {
+  return string.replace(/[\r\n]+/gm, ' ').replace(/\s+/gm, '');
+}
 
-    return nn(contents);
+function readFile(file) {
+  const contents = fs.readFileSync(path.join(__dirname, file), 'utf8');
+
+  return nn(contents);
 }
 
 function fileExists(file) {
-    return fs.existsSync(path.join(__dirname, file));
+  return fs.existsSync(path.join(__dirname, file));
 }
 
 describe('critical', () => {
-    it('generates minified critical-path CSS successfully', () => {
-        const expected = readFile('expected/critical.css');
-        const output = readFile('generated/critical.css');
+  it('generates minified critical-path CSS successfully', () => {
+    const expected = readFile('expected/critical.css');
+    const output = readFile('generated/critical.css');
 
-        expect(output).to.equal(expected);
-    });
+    expect(strip(output)).to.equal(strip(expected));
+  });
 
-    it('generates html with minified critical-path CSS successfully', () => {
-        const expected = readFile('expected/index-minified.html');
-        const output = readFile('generated/index-critical.html');
+  it('generates html with minified critical-path CSS successfully', () => {
+    const expected = readFile('expected/index-minified.html');
+    const output = readFile('generated/index-critical.html');
 
-        expect(output).to.equal(expected);
-    });
+    expect(strip(output)).to.equal(strip(expected));
+  });
 
-    it('generates html with minified critical-path CSS successfully ignoring bootstrap css', () => {
-        const expected = readFile(
-            'expected/index-minified-ignore-bootstrap.html'
-        );
-        const output = readFile(
-            'generated/index-critical-ignore-bootstrap.html'
-        );
+  it('generates html with minified critical-path CSS successfully ignoring bootstrap css', () => {
+    const expected = readFile('expected/index-minified-ignore-bootstrap.html');
+    const output = readFile('generated/index-critical-ignore-bootstrap.html');
 
-        expect(output).to.equal(expected);
-    });
+    expect(strip(output)).to.equal(strip(expected));
+  });
 
-    it('generates html with minified critical-path CSS for multiple dimensions successfully', () => {
-        const expected = readFile('expected/index-dimensions.html');
-        const output = readFile('generated/index-dimensions.html');
+  it('generates html with minified critical-path CSS for multiple dimensions successfully', () => {
+    const expected = readFile('expected/index-dimensions.html');
+    const output = readFile('generated/index-dimensions.html');
 
-        expect(output).to.equal(expected);
-    });
+    expect(strip(output)).to.equal(strip(expected));
+  });
 
-    it('generates html with extracted minified critical-path CSS successfully', () => {
-        const expected = readFile('expected/index-extract.html');
-        const output = readFile('generated/index-critical-extract.html');
+  it('generates html with extracted minified critical-path CSS successfully', () => {
+    const expected = readFile('expected/index-extract.html');
+    const output = readFile('generated/index-critical-extract.html');
 
-        expect(output).to.equal(expected);
-        expect(fileExists('fixture/styles/main.d41d8cd9.css')).to.equal(true);
-        expect(fileExists('fixture/styles/bootstrap.130e6d81.css')).to.equal(
-            true
-        );
-    });
+    expect(strip(output)).to.equal(strip(expected));
+    expect(fileExists('fixture/styles/main.d41d8cd9.css')).to.equal(true);
+    expect(fileExists('fixture/styles/bootstrap.130e6d81.css')).to.equal(true);
+  });
 
-    it('generates multiple html files without throwing "warning: possible EventEmitter memory leak detected"', () => {
-        const expected = readFile('expected/index-multiple.html');
-        let output;
+  it('generates multiple html files without throwing "warning: possible EventEmitter memory leak detected"', () => {
+    const expected = readFile('expected/index-multiple.html');
+    let output;
 
-        for (let i = 1; i <= 5; i++) {
-            output = readFile(`generated/multiple/index${i}.html`);
-            expect(output).to.equal(
-                expected.replace(
-                    '<title>page x</title>',
-                    `<title>page ${i}</title>`
-                )
-            );
-        }
-    });
+    for (let i = 1; i <= 5; i++) {
+      output = readFile(`generated/multiple/index${i}.html`);
+      expect(strip(output)).to.equal(strip(expected.replace('<title>page x</title>', `<title>page ${i}</title>`)));
+    }
+  });
 
-    it('generates multiple html files with minified CSS', () => {
-        const expected = readFile('expected/index-multiple-minified.html');
-        let output;
+  it('generates multiple html files with minified CSS', () => {
+    const expected = readFile('expected/index-multiple-minified.html');
+    let output;
 
-        for (let i = 1; i <= 5; i++) {
-            output = readFile(`generated/multiple-min/index${i}.html`);
-            expect(output).to.equal(
-                expected.replace(
-                    '<title>page x</title>',
-                    `<title>page ${i}</title>`
-                )
-            );
-        }
-    });
+    for (let i = 1; i <= 5; i++) {
+      output = readFile(`generated/multiple-min/index${i}.html`);
+      expect(strip(output)).to.equal(strip(expected.replace('<title>page x</title>', `<title>page ${i}</title>`)));
+    }
+  });
 
-    it('should keep external URLs with extract option', () => {
-        const expected = readFile('expected/index-external.html');
-        const output = readFile('generated/index-external.html');
+  it('should keep external URLs with extract option', () => {
+    const expected = readFile('expected/index-external.html');
+    const output = readFile('generated/index-external.html');
 
-        expect(output).to.equal(expected);
-        expect(fileExists('fixture/styles/main.d41d8cd9.css')).to.equal(true);
-        expect(fileExists('fixture/styles/bootstrap.ca3dbc40.css')).to.equal(
-            true
-        );
-    });
+    expect(strip(output)).to.equal(strip(expected));
+    expect(fileExists('fixture/styles/main.d41d8cd9.css')).to.equal(true);
+    expect(fileExists('fixture/styles/bootstrap.ca3dbc40.css')).to.equal(true);
+  });
 
-    it('should write files to folder when folder is specified as dest', () => {
-        const expected = readFile('expected/index-multiple.html');
-        let output;
+  it('should write files to folder when folder is specified as dest', () => {
+    const expected = readFile('expected/index-multiple.html');
+    let output;
 
-        for (let i = 1; i <= 5; i++) {
-            expect(
-                fileExists(`generated/multiple-files-folder/index${i}.html`)
-            ).to.equal(true);
-            output = readFile(`generated/multiple-files-folder/index${i}.html`);
-            expect(output).to.equal(
-                expected.replace(
-                    '<title>page x</title>',
-                    `<title>page ${i}</title>`
-                )
-            );
-        }
-    });
+    for (let i = 1; i <= 5; i++) {
+      expect(fileExists(`generated/multiple-files-folder/index${i}.html`)).to.equal(true);
+      output = readFile(`generated/multiple-files-folder/index${i}.html`);
+      expect(strip(output)).to.equal(strip(expected.replace('<title>page x</title>', `<title>page ${i}</title>`)));
+    }
+  });
 
-    it('should consider ignore option', () => {
-        const expected = readFile('expected/critical-ignore.css');
-        const output = readFile('generated/critical-ignore.css');
+  it('should consider ignore option', () => {
+    const expected = readFile('expected/critical-ignore.css');
+    const output = readFile('generated/critical-ignore.css');
 
-        expect(output).to.equal(expected);
-    });
+    expect(strip(output)).to.equal(strip(expected));
+  });
 
-    it('should allow glob pattern for css option', () => {
-        const expected = readFile('expected/critical.css');
-        const output = readFile('generated/critical-glob.css');
+  it('should allow glob pattern for css option', () => {
+    const expected = readFile('expected/critical.css');
+    const output = readFile('generated/critical-glob.css');
 
-        expect(output).to.equal(expected);
-    });
+    expect(strip(output)).to.equal(strip(expected));
+  });
 
-    it('should allow remote sources', () => {
-        const expectedCss = readFile('expected/critical.css');
-        const expectedHtml = readFile('expected/index-minified.html');
-        const outputCss = readFile('generated/remote.css');
-        const outputHtml = readFile('generated/remote.html');
+  it('should allow remote sources', () => {
+    const expectedCss = readFile('expected/critical.css');
+    const expectedHtml = readFile('expected/index-minified.html');
+    const outputCss = readFile('generated/remote.css');
+    const outputHtml = readFile('generated/remote.html');
 
-        expect(outputCss).to.equal(expectedCss);
-        expect(outputHtml).to.equal(expectedHtml);
-    });
+    expect(strip(outputCss)).to.equal(strip(expectedCss));
+    expect(strip(outputHtml)).to.equal(strip(expectedHtml));
+  });
 });
